ad4731b3e8280916aa824e7785dd5bcf71259009
Asankhaya Sharma <asankhaya@yahoo.com>
2014-08-02 12:28:51 +0800

    ignore some more words with special characters

--- SpellChecker.js
+++ SpellChecker.js
@@ -161,7 +161,7 @@ var page = require('webpage').create();
               }
               //console.log(JSON.stringify(ua));
               var ual = ua.links;
-              var words = ua.text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()"”?\]\[—]/g," ").match(/\S+/g);
+              var words = ua.text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`’@~()"”?\]\[—]/g," ").match(/\S+/g);
               var i;
               var wrongwords = [];
               function isNumeric(obj) {
