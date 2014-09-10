c4423d159b5c7d0af9f98af40375d621086f24de
akashsharma91 <akash.sharma@joshtechnologygroup.com>
2014-09-10 12:09:27 -0700

    screen checker mod1 and mod2 added

--- ScreenChecker.js
+++ ScreenChecker.js
@@ -1,34 +1,84 @@
 // LinkChecker for AutoBot
 var system = require('system');
+var page = require('webpage').create();
+var sizearr = [[1600 ,990],[1280, 800],[768, 1024],[320, 480]];
 var URL = "";
-var arrLinks = [{link:'/',text:'__root__',parent:'__init__'}];
-var visitedLinks = [];
-var maxLinks = 100; // maximum number of links to be checked, exits after that
-var skipExternal = false; // skip external links while checking, useful to test locally
-var followall = false; // follow all links, will make you crawl outside your website
 var debug = true; // prints a list of all urls checked
 var verbose = false; // prints detailed arrays showing visited links and queued links
+var cleanURL;
+var count = 3;
+
+page.onError = function(msg, trace) {
+    if(debug && verbose) {
+        var msgStack = ['ERROR: ' + msg];
+        if (trace && trace.length) {
+            msgStack.push('TRACE:');
+            trace.forEach(function(t) {
+                msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+            });
+        }
+        console.error(msgStack.join('\n'));
+    }
+};
+
+var defaulScreenShots = function() {
+    takeScreenShot(sizearr[count][0], sizearr[count][1], count);
+}
+var takeScreenShot = function (pagewidth, pageheight, count) {
+    if(typeof(count)==='undefined') {
+        count = 0;
+    }
+    page.viewportSize = {
+        width: pagewidth,
+        height: pageheight
+    };
+    page.open(URL, function(status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+            phantom.exit();
+        }
+        else {
+            console.log(status, pagewidth, pageheight, count);
+            page.render(cleanURL+'/'+pagewidth+'x'+pageheight+'.png');
+            if(count === 0) {
+                setTimeout(function(){
+                    phantom.exit();
+                },1000);
+            }
+            else {
+                setTimeout(function(){
+                    count--;
+                    takeScreenShot(sizearr[count][0], sizearr[count][1], count);
+                },3000);
+            }
+        }
+    });
+}
+
 
 if (system.args.length === 1) {
     console.log('Pass the URL of the website as argument to this script!');
-    phantom.exit();
+    phantom.exit(1);
 } 
 else {
     URL = system.args[1];
-    if (typeof String.prototype.endsWith !== 'function' ) {
-        String.prototype.endsWith = function( str ) {
-            return this.substring( this.length - str.length, this.length ) === str;
-        };
-    } 
-    if(URL.endsWith(".html")) { //todo : replace with a regex later
-        arrLinks = [{link:'',text:'__root__',parent:'__'}];
-    }
-    if(system.args.length > 2) {
-        maxLinks = system.args[2];
+    cleanURL = URL.replace(/.*?:\/\//g, "");
+    switch(system.args.length) {
+        case 2:
+            defaulScreenShots();
+            break;
+        case 3:
+            var customSize = system.args[2].split('x');
+            if( customSize[0] < 250 || customSize[1] < 250 || customSize[0] > 2048 || customSize[1] > 2048 ) {
+                console.log('Pass the size as 1600x900 without whitespace');
+                phantom.exit();
+            }
+            takeScreenShot(customSize[0],customSize[1]);
+            break;
+        case 4:
+            break;  
+        default:
+            phantom.exit();
+        break;
     }
 }
-var page = require('webpage').create();
-page.open(URL, function() {
-    page.render(URL+'.png');
-    phantom.exit();
-}); 
