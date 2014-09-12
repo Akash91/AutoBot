492c3e9019607dbbbf071bd33967dd7a182533e5
Akash91 <akash1991@gmail.com>
2014-09-12 03:07:33 -0700

    Adding page close and some more changes

--- ScreenChecker.js
+++ ScreenChecker.js
@@ -1,25 +1,13 @@
 // LinkChecker for AutoBot
 var system = require('system');
-var page = require('webpage').create();
-var sizearr = [[1600 ,990],[1280, 800],[768, 1024],[320, 480]];
+var webpage = require('webpage');
+var sizearr = [[320 ,480],[768, 1024],[1280, 800],[1600, 990]];
 var URL = "";
 var debug = true; // prints a list of all urls checked
 var verbose = false; // prints detailed arrays showing visited links and queued links
 var cleanURL;
 var count = 3;
-
-page.onError = function(msg, trace) {
-    if(debug && verbose) {
-        var msgStack = ['ERROR: ' + msg];
-        if (trace && trace.length) {
-            msgStack.push('TRACE:');
-            trace.forEach(function(t) {
-                msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
-            });
-        }
-        console.error(msgStack.join('\n'));
-    }
-};
+var screentimeout = 3000;
 
 var defaulScreenShots = function() {
     takeScreenShot(sizearr[count][0], sizearr[count][1], count);
@@ -28,6 +16,35 @@ var takeScreenShot = function (pagewidth, pageheight, count) {
     if(typeof(count)==='undefined') {
         count = 0;
     }
+    var page = webpage.create();
+    page.onError = function(msg, trace) {
+        if(debug && verbose) {
+            var msgStack = ['ERROR: ' + msg];
+            if (trace && trace.length) {
+                msgStack.push('TRACE:');
+                trace.forEach(function(t) {
+                    msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+                });
+            }
+            console.error(msgStack.join('\n'));
+        }
+    };
+    page.onLoadFinished = function(status) {
+        console.log("Generating screenshot for "+pagewidth+"x"+pageheight);
+        page.render(cleanURL+'/'+pagewidth+'x'+pageheight+'.png');
+        if(count === 0) {
+            setTimeout(function(){
+                phantom.exit();
+            },1000);
+        }
+        else {
+            setTimeout(function(){
+                count--;
+                takeScreenShot(sizearr[count][0], sizearr[count][1], count);
+                page.close();
+            },screentimeout);
+        }
+    };
     page.viewportSize = {
         width: pagewidth,
         height: pageheight
@@ -37,24 +54,8 @@ var takeScreenShot = function (pagewidth, pageheight, count) {
             console.log('Unable to load the address!');
             phantom.exit();
         }
-        else {
-            console.log(status, pagewidth, pageheight, count);
-            page.render(cleanURL+'/'+pagewidth+'x'+pageheight+'.png');
-            if(count === 0) {
-                setTimeout(function(){
-                    phantom.exit();
-                },1000);
-            }
-            else {
-                setTimeout(function(){
-                    count--;
-                    takeScreenShot(sizearr[count][0], sizearr[count][1], count);
-                },3000);
-            }
-        }
     });
 }
-
 
 if (system.args.length === 1) {
     console.log('Pass the URL of the website as argument to this script!');
@@ -69,10 +70,6 @@ else {
             break;
         case 3:
             var customSize = system.args[2].split('x');
-            if( customSize[0] < 250 || customSize[1] < 250 || customSize[0] > 2048 || customSize[1] > 2048 ) {
-                console.log('Pass the size as 1600x900 without whitespace');
-                phantom.exit();
-            }
             takeScreenShot(customSize[0],customSize[1]);
             break;
         default:
